d013d851b899e9eaba9914cef5f2c66693d60487
Dean Herbert <pe@ppy.sh>
2020-09-19 20:55:35 +0900

    Initial commit

---
+++ GlobalRankLookupCache/Controllers/RankLookupController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GlobalRankLookupCache.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RankLookupController : ControllerBase
+    {
+        private readonly ILogger<RankLookupController> _logger;
+
+        public RankLookupController(ILogger<RankLookupController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int Get()
+        {
+            return 1234;
+        }
+    }
+}
